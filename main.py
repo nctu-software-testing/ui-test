96ec561486e3b54b0bca965440f695c0598a3b4c
jhcheng <jenhao.cs08g@nctu.edu.tw>
2020-06-10 00:49:47 +0800

    add TC06, TC07, TC08 and Run 8 tests in 148.062s OK

--- main.py
+++ main.py
@@ -19,6 +19,13 @@ custo_addr = "c"
 custo_pwd = "c"
 bm_addr = "b"
 bm_pwd = "b"
+# 20% 10% off, no discount
+discount_20_code = "5F08-C0F0-0180-DB00"
+discount_10_code = "590F-10E0-1260-B00B"
+discount_no_code = "XXXX-YYYY-ZZZZ-AAAA"
+
+# for location
+location_address = '大學路1001號'
 
 class UiTest(unittest.TestCase):
     def setUp(self):
@@ -172,17 +179,78 @@ class UiTest(unittest.TestCase):
 
     # TC 06- Verify that discount state is correct.
     def test_discount_code(self):
-        pass
+        self.login(custo_addr, custo_pwd)
+        driver = self.driver
+        driver.get(TARGET+'')
+        driver.get(TARGET+'/products/item/63')
+        driver.execute_script("arguments[0].click();", driver.find_elements_by_xpath('/html/body/main/div/div/div[1]/div[2]/div/div[3]/button')[0])
+        
+        driver.get(TARGET+'/shopping-cart')
+        # type 20% off code 
+        driver.find_element_by_xpath('//*[@id="discount"]').send_keys(discount_20_code)
+        # click finish
+        driver.execute_script("arguments[0].click();", driver.find_elements_by_xpath('//*[@id="discountBtn"]')[0])
+        sleep(3)
+        # assert discount correct
+        price = int((driver.find_element_by_xpath('//*[@id="checkout_info"]/div[1]/table/tbody/tr[1]/td[2]/h5').text)[2:])
+        discount_price = int((driver.find_element_by_xpath('//*[@id="discountValue"]').text))
+        self.assertEqual(discount_price, int(price*(-0.2)))
+
+        # type wrong code 
+        driver.find_element_by_xpath('//*[@id="discount"]').send_keys(discount_no_code)
+        # click finish
+        driver.execute_script("arguments[0].click();", driver.find_elements_by_xpath('//*[@id="discountBtn"]')[0])
+        sleep(3)
+        # assert discount correct
+        discount_price = int(driver.find_element_by_xpath('//*[@id="discountValue"]').text)
+        self.assertEqual(discount_price, 0)
+
 
     # TC 07- Verify that updating the email. 
     def test_update_email(self):
-        pass
+        self.login(custo_addr, custo_pwd)
+        driver = self.driver
+        driver.get(TARGET+'/profile')
+        
+        # click modify button
+        driver.execute_script("arguments[0].click();", driver.find_elements_by_xpath('/html/body/main/div/div/div[2]/div/div/div/div/ul/li[5]/span[2]/a')[0])
+        sleep(3)
+        # type email
+        email = "asdf@gzxcv.qwer"
+        driver.find_element_by_xpath('//*[@id="emailInput"]').send_keys(email)
+        # click confirm
+        driver.execute_script("arguments[0].click();", driver.find_elements_by_xpath('/html/body/div[10]/div/div/div[2]/form/div[2]/button')[0])
+        sleep(2)
+        # get new profile email
+        profile_email = (driver.find_element_by_xpath('/html/body/main/div/div/div[2]/div/div/div/div/ul/li[5]/span[2]').text).split(' ')[0]
+        self.assertEqual(profile_email, email)
 
     # TC 08- Verify that adding a new customer address.
     def test_add_address(self):
-        pass
+        self.login(custo_addr, custo_pwd)
+        driver = self.driver
+        driver.get(TARGET+'/location')
 
+        # click add address button
+        driver.execute_script("arguments[0].click();", driver.find_elements_by_xpath('//*[@id="newlocation"]')[0])
+        city_ele = driver.find_element_by_xpath('//*[@id="twzipcode"]/div[1]/input')
+        area_ele = driver.find_element_by_xpath('//*[@id="twzipcode"]/div[2]/input')
+        address_ele = driver.find_element_by_xpath('//*[@id="lo"]/form/input[1]')
+        for i in range(5):
+            city_ele.send_keys(Keys.DOWN)
+        city_ele.send_keys(Keys.RETURN)
+        address_ele.send_keys(location_address)
+        
+        # click save
+        driver.execute_script("arguments[0].click();", driver.find_elements_by_xpath('//*[@id="lo"]/form/button[1]')[0])
 
+        # get last address
+        last_index = len(driver.find_elements_by_xpath("/html/body/main/div/div/div[2]/div/div/table/tbody/tr"))
+        last_address = driver.find_element_by_xpath('/html/body/main/div/div/div[2]/div/div/table/tbody/tr['+ str(last_index) +']/td[2]').text
+
+        # check        
+        self.assertEqual(last_address, location_address)
+        
 def main():
     unittest.main()
 
