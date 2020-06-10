7fc5b19347b7fc3a9a3a77c533b1b6db61dc63e8
jhcheng <jenhao.cs08g@nctu.edu.tw>
2020-06-10 20:26:00 +0800

    add script

--- auto.sh
+++ auto.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "UI Test"
+
+cp db-template.db tests/resources/db-template.db
+php artisan serve --env=ui.test &
 ./main.py -v
+# jobs -l | grep "php artisan serve" | awk '{print $2}' | xargs kill -9
+ps aux | grep "db-project/server.php" | head -1 | awk '{print $2}' | xargs kill -9
